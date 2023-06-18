5215043186f4ebfa1473103bd3c4ec5fafaae46d
Bigari B <bigari7@gmail.com>
2023-06-18 12:03:04 +0100

    Improve ubuntu-airbyte-install.sh

--- ubuntu-airbyte-install.sh
+++ ubuntu-airbyte-install.sh
@@ -1,3 +1,14 @@
 #!usr/bin/bash
 
-bash <(curl -s https://raw.githubusercontent.com/airbytehq/airbyte/master/run-ab-platform.sh)
+BASE_URL=https://raw.githubusercontent.com/airbytehq/airbyte/master/
+
+mkdir airbyte
+
+# Any string is an array to POSIX shell. Space seperates values
+all_files="run-ab-platform.sh docker-compose.yaml docker-compose.debug.yaml .env .env.dev flags.yml"
+
+for file in $all_files; do
+  curl "$BASE_URL/$file" > "airbyte/$file"
+done
+
+bash airbyte/run-ab-platform.sh
