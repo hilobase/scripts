2bce2fe0dbb9b07f7482fc05a3e12114b31c268c
Bigari B <bigari7@gmail.com>
2023-06-18 12:48:14 +0100

    Simplify ubuntu-airbyte-install.sh

--- ubuntu-airbyte-install.sh
+++ ubuntu-airbyte-install.sh
@@ -1,16 +1,7 @@
 #!usr/bin/bash
 
-BASE_URL=https://raw.githubusercontent.com/airbytehq/airbyte/master
-
 mkdir airbyte
-
-# Any string is an array to POSIX shell. Space seperates values
-all_files="run-ab-platform.sh docker-compose.yaml docker-compose.debug.yaml .env .env.dev flags.yml"
-
-for file in $all_files; do
-  curl "$BASE_URL/$file" > "airbyte/$file"
-done
-
-echo "Changing directory to ./airbyte and running ab-platform"
+echo "Changing directory to ./airbyte"
 cd airbyte
+curl https://raw.githubusercontent.com/airbytehq/airbyte/master/run-ab-platform.sh > run-ab-platform.sh
 bash run-ab-platform.sh
